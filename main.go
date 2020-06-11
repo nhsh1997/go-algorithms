2e8e0ac6f9e539d999b9bca68cca40cb4d85d828
Hoang Nguyen <nhsh1997@gmail.com>
2020-06-11 13:58:47 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,10 +4,27 @@ import "fmt"
 import "github.com/nhsh1997/go-algorithms/sorting/bubble-sort"
 
 func main()  {
-	var numbers []int = []int{33, 91, 76, 8, 22}
-	var ascending bool = false
+	var indices [2]int
+	var given_nums []int = []int{11, 2, 7, 15}
+	var target int = 17
+	N := len(given_nums)
+	fmt.Println("Original list: ", given_nums)
 
-	fmt.Println("Original list: ", numbers)
-	bubble_sort.Sort(numbers, ascending)
-	fmt.Println("Sorted list: ", numbers)
+	bubble_sort.Sort(given_nums, true)
+
+
+	fmt.Println("Sorted list: ", given_nums)
+
+	for i := 0; i < N - 1; i ++  {
+		if given_nums[i] < target {
+			for j := i + 1; j < N ; j++ {
+				if (given_nums[i] + given_nums[j]) == target {
+					indices[0], indices[1] = i, j
+					break
+				}
+			}
+		}
+	}
+
+	fmt.Println("Indices: ", indices)
 }
