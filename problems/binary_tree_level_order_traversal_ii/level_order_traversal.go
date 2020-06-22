5c18f1db778c44a2284fc244978670ed40999c8a
Hoang Nguyen <nhsh1997@gmail.com>
2020-06-22 13:14:08 +0700

    update

---
+++ problems/binary_tree_level_order_traversal_ii/level_order_traversal.go
@@ -0,0 +1,77 @@
+package binary_tree_level_order_traversal_ii
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrderBottom(root *TreeNode) [][]int {
+	mapResult := make(map[int][]int)
+	depth := 0
+	explore(root, 1, &depth, mapResult)
+	if depth == 0 {
+		return [][]int{}
+	} else {
+		result := make([][]int, depth)
+		for key, arr := range mapResult{
+			result[depth - key] = arr
+		}
+		return result
+	}
+}
+
+func explore(node *TreeNode, level int, depth *int, mapResult map[int][]int)  {
+	if node == nil {
+		return
+	} else {
+		if level > *depth {
+			*depth = level
+		}
+		arr, ok := mapResult[level]
+		if !ok {
+			mapResult[level] = []int{}
+		}
+		mapResult[level] = append(arr, node.Val)
+	}
+
+	if node.Left != nil {
+		explore(node.Left, level + 1, depth, mapResult)
+	}
+	if node.Right != nil {
+		explore(node.Right, level + 1, depth, mapResult)
+	}
+}
+
+
+
+/*func main()  {
+	root := &TreeNode{
+		Val:   1,
+		Left:  &TreeNode{
+			Val:   2,
+			Left:  &TreeNode{
+				Val:   4,
+				Left:  nil,
+				Right: nil,
+			},
+			Right: &TreeNode{
+				Val:   5,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+		Right: &TreeNode{
+			Val:   3,
+			Left:  nil,
+			Right: nil,
+		},
+	}
+
+	result := levelOrderBottom(root)
+	fmt.Println(result)
+}
+
+
+
+*/
--- searching/binary_search/binary_search.go
+++ searching/binary_search/binary_search.go
@@ -1,7 +1,37 @@
 package binary_search
 
-func BinarySearch(list []int, left int, right int, x int)  {
-	if left >= right {
-		
+func BinarySearch(list []int, left int, right int, x int) int {
+	if right >= left {
+		mid := left + (right - left) / 2
+
+		if list[mid] == x {
+			return mid
+		} else {
+			if mid == 0 {
+				return -1
+			}
+		}
+
+		if list[mid] > x {
+			return BinarySearch(list, left, mid - left, x)
+		}
+
+		return  BinarySearch(list, mid + 1, right, x)
+	}
+	return -1
+}
+
+
+/*func main()  {
+	list := []int{2, 4, 6, 9, 10}
+	N := len(list)
+	x := 4
+	result := BinarySearch(list, 0, N - 1, x)
+
+	if result != -1 {
+		fmt.Println(fmt.Sprintf("Index of ", x, " is ", result))
+	} else {
+		fmt.Println(fmt.Sprintf("Not found ", x))
 	}
 }
+*/
